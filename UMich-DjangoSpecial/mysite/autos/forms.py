53b08daac6ce27720119e861926f41f4ea19dccc
“mj0-git” <meraj0.patel@gmail.com>
2022-01-22 18:05:09 -0500

    cleanup

--- UMich-DjangoSpecial/mysite/autos/forms.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import fields
-from django.forms import ModelForm
-from autos.models import Make
-
-class MakeForm(ModelForm):
-    class Meta: 
-        model = Make
-        fields = '__all__'
